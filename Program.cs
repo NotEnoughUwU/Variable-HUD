dc49586c871de36c7a8b5fdc11128ef3c5071aaa
NotEnoughUwU <92890554+NotEnoughUwU@users.noreply.github.com>
2022-09-20 08:48:57 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variable_HUD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lives = 3;
+            int health = 100;
+            int score = 0;
+            float multiplier = 1.0f;
+            int level = 1;
+            string[] levelMessages =
+            {
+
+            };
+        }
+        static void DisplayText()
+        {
+            Console.Clear();
+
+            Console.WriteLine("======== Super Real Game That Exists !!! ========");
+            Console.WriteLine("Level: " + level + "Lives: " + lives + "Health: " + health + "Score: " + score + "Multiplier: " + multiplier);
+        }
+    }
+}
